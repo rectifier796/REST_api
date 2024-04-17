bf7cc09879237f6cda6750825bbdf4d09b8f93c2
ayushsingh9919 <ayush.singh@maximl.com>
2024-04-17 12:27:33 +0530

    Last commit

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import path from 'node:path';
+import cloudinary from "../config/cloudinary";
 
 export const createBook = async (
   req: Request,
@@ -7,6 +9,28 @@ export const createBook = async (
   next: NextFunction
 ) => {
   try {
+
+    if(!req.files){
+        return next(createHttpError(400,"Cover Image Requires"));
+    }
+
+    const files= req.files as {[fieldname:string]: Express.Multer.File[]};
+
+    console.log(files);
+
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(__dirname, '../../public/data/uploads',fileName);
+    console.log(filePath,fileName,coverImageMimeType);
+    const uploadResult =await cloudinary.uploader.upload(filePath,{
+        filename_override: fileName,
+        folder:"book-covers",
+        format: coverImageMimeType,
+    })
+
+    console.log(uploadResult);
+
+    res.json({});
     
   } catch (err) {
     console.log(err);
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,11 @@ const _config = {
     port: process.env.PORT,
     databaseUrl: process.env.MONGO_CONNECTION_STRING,
     env: process.env.NODE_ENV,
-    jwtSecret: process.env.JWT_SECRET
+    jwtSecret: process.env.JWT_SECRET,
+    cloudName: process.env.CLOUD_NAME,
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET
 };
+
 
 export const config = Object.freeze(_config);
